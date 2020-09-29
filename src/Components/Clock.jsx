d15d9610db9b0903bf0e4374529f5edd6bd852b2
Anthony Ilersich <antonio.ilersich@gmail.com>
2020-09-29 15:37:06 -0400

    fix clock for bad input

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text } from './Text';
 
+const format = number => Number.isNaN(number)
+	? '??'
+	: Math.floor(number).toString().padStart(2, '0');
+
 export const Clock = ({ ms }) => (
-	<Text>{
-		Math.floor(ms / 1000 / 60 / 60).toString().padStart(2, '0')
-	}:{
-		Math.floor(ms / 1000 / 60 % 60).toString().padStart(2, '0')
-	}:{
-		Math.floor(ms / 1000 % 60).toString().padStart(2, '0')
-	}</Text>
+	<Text>
+		{format(ms / 1000 / 60 / 60)}:{format(ms / 1000 / 60 % 60)}:{format(ms / 1000 % 60)}
+	</Text>
 );
