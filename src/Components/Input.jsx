aa9ef45f05fd395ee25f9cdd2d50a9a2f782608a
Anthony Ilersich <antonio.ilersich@gmail.com>
2020-09-29 15:37:40 -0400

    update input style

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -4,7 +4,13 @@ export const Input = styled.input`
 	display: flex;
 	flex-grow: 1;
 	flex-shrink: 1;
-	height: 1.2em;
-	margin: 4px;
+	height: 1em;
+	margin: 0 2px;
 	align-self: center;
+	padding: none;
+	padding-top: 8px;
+	border: none;
+	border-bottom: 1px solid #b0b0b0;
+	outline: none;
+	font-size: 16px;
 `;
